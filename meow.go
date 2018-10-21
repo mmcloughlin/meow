9b12799fca6de8af317a84786080cbe2b51e4375
Michael McLoughlin <mmcloughlin@gmail.com>
2018-10-20 17:52:10 -0700

    wip

---
+++ meow.go
@@ -0,0 +1,20 @@
+package meow
+
+// BlockSize is the underlying block size of Meow hash in bytes.
+const BlockSize = 256
+
+// Size of a Meow checksum in bytes.
+const Size = 16
+
+// block hashes one Meow block.
+func block(state *byte, src []byte)
+
+// sum computes the Meow checksum of data and writes to dst.
+func sum(seed uint64, dst *byte, src []byte)
+
+// Sum returns the Meow checksum of data.
+func Sum(seed uint64, data []byte) [Size]byte {
+	var dst [Size]byte
+	sum(seed, &dst[0], data)
+	return dst
+}
