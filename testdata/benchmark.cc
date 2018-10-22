84b8ccaf0b4b0913b47b304374733770f67c5a30
Michael McLoughlin <mmcloughlin@gmail.com>
2018-10-21 18:37:57 -0700

    reformat

--- testdata/benchmark.cc
+++ testdata/benchmark.cc
@@ -5,18 +5,19 @@
 #include <immintrin.h>
 #include "meow_hash.h"
 
-static void meow_hash_benchmark(benchmark::State& state) {
-    size_t            buffer_size = state.range(0);
-    std::vector<char> buffer(buffer_size, 0);
+static void meow_hash_benchmark(benchmark::State &state)
+{
+  size_t buffer_size = state.range(0);
+  std::vector<char> buffer(buffer_size, 0);
 
-    for (auto _ : state)
-    {
-      benchmark::DoNotOptimize(MeowHash1(0, buffer_size, buffer.data()));
-    }
+  for (auto _ : state)
+  {
+    benchmark::DoNotOptimize(MeowHash1(0, buffer_size, buffer.data()));
+  }
 
-    state.SetBytesProcessed(state.iterations() * buffer_size);
+  state.SetBytesProcessed(state.iterations() * buffer_size);
 }
 
-BENCHMARK(meow_hash_benchmark)->RangeMultiplier(2)->Range(1, 1<<24);
+BENCHMARK(meow_hash_benchmark)->RangeMultiplier(2)->Range(1, 1 << 24);
 
 BENCHMARK_MAIN();
