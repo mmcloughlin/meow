e2bf2bbe359a5c8841d2290a22e75b61f500d70b
Michael McLoughlin <mmcloughlin@gmail.com>
2018-10-22 19:50:50 -0700

    update benchmark
    Address comments from @dgryski
    
    - use pre-allocated buffer in benchmark
    - accumate hashes in a global sink variable

--- benchmark_test.go
+++ benchmark_test.go
@@ -7,20 +7,27 @@ import (
 	"github.com/mmcloughlin/meow"
 )
 
+// buffer is pre-allocated to avoid allocations in benchmarking functions themselves.
+var buffer = make([]byte, 1<<20)
+
+// sink is a running sum of checksum output, to ensure the benchmarked function isn't optimized away.
+var sink byte
+
 func BenchmarkChecksum(b *testing.B) {
 	var sizes []int
-	for n := uint(1); n <= 20; n++ {
+	for n := uint(0); n <= 20; n++ {
 		sizes = append(sizes, 1<<n)
 	}
 
 	for _, size := range sizes {
 		name := fmt.Sprintf("size=%d", size)
 		b.Run(name, func(b *testing.B) {
-			data := make([]byte, size)
-			for i := 0; i < b.N; i++ {
-				_ = meow.Checksum(0, data)
-			}
+			data := buffer[:size]
 			b.SetBytes(int64(size))
+			for i := 0; i < b.N; i++ {
+				s := meow.Checksum(0, data)
+				sink += s[0]
+			}
 		})
 	}
 }
