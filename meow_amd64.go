21b5fbf33f9b329d9bd525d1e822ab5d3e728c36
Michael McLoughlin <mmcloughlin@gmail.com>
2018-10-26 20:24:20 -0700

    wip

--- meow_amd64.go
+++ meow_amd64.go
@@ -2,14 +2,16 @@
 
 package meow
 
-import "golang.org/x/sys/cpu"
-
 func init() {
-	// AVX required for VEX-encoded AES instruction, which allows non-aligned memory addresses.
-	if cpu.X86.HasAES && cpu.X86.HasAVX {
-		checksum = checksum128
-	}
+	//// AVX required for VEX-encoded AES instruction, which allows non-aligned memory addresses.
+	//if cpu.X86.HasAES && cpu.X86.HasAVX {
+	//	checksum = checksum128
+	//}
+	checksum = checksum512
 }
 
-// checksum128 implements Meow checksum in assembly.
+// checksum128 implements Meow checksum with AES-NI.
 func checksum128(seed uint64, dst, src []byte)
+
+// checksum512 implements Meow checksum with VAES-512.
+func checksum512(seed uint64, dst, src []byte)
