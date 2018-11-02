657a7712b1fe50cb79482ac5d6eab91e12efcc3d
Michael McLoughlin <mmcloughlin@gmail.com>
2018-11-02 12:52:57 -0700

    benchmark for hash.Hash

--- benchmark_test.go
+++ benchmark_test.go
@@ -13,7 +13,10 @@ var buffer = make([]byte, 1<<20)
 // sink is a running sum of checksum output, to ensure the benchmarked function isn't optimized away.
 var sink byte
 
-func BenchmarkChecksum(b *testing.B) {
+// benchmarkTarget is a hash function to be benchmarked.
+type benchmarkTarget func([]byte) byte
+
+func benchmarkChecksum(b *testing.B, f benchmarkTarget) {
 	var sizes []int
 	for n := uint(0); n <= 20; n++ {
 		sizes = append(sizes, 1<<n)
@@ -25,9 +28,23 @@ func BenchmarkChecksum(b *testing.B) {
 			data := buffer[:size]
 			b.SetBytes(int64(size))
 			for i := 0; i < b.N; i++ {
-				s := meow.Checksum(0, data)
-				sink += s[0]
+				sink += f(data)
 			}
 		})
 	}
+}
+
+func BenchmarkChecksum(b *testing.B) {
+	benchmarkChecksum(b, func(data []byte) byte {
+		h := meow.Checksum(0, data)
+		return h[0]
+	})
+}
+
+func BenchmarkHash(b *testing.B) {
+	benchmarkChecksum(b, func(data []byte) byte {
+		h := meow.New(0)
+		h.Write(data)
+		return h.Sum(nil)[0]
+	})
 }
