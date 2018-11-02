7690824d9a74fe3c762ba0883778979229071a53
Michael McLoughlin <mmcloughlin@gmail.com>
2018-11-02 12:59:50 -0700

    add example for New()

--- example_test.go
+++ example_test.go
@@ -2,6 +2,7 @@ package meow_test
 
 import (
 	"fmt"
+	"io"
 
 	"github.com/mmcloughlin/meow"
 )
@@ -9,5 +10,13 @@ import (
 func ExampleChecksum() {
 	checksum := meow.Checksum(0, []byte("Hello, World!"))
 	fmt.Printf("%x\n", checksum)
+	// Output: a8cfb4aad7eada8ef007aafe27135386
+}
+
+func ExampleNew() {
+	h := meow.New(0)
+	io.WriteString(h, "Hello, ")
+	io.WriteString(h, "World!")
+	fmt.Printf("%x\n", h.Sum(nil))
 	// Output: a8cfb4aad7eada8ef007aafe27135386
 }
